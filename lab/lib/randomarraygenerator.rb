5946239c8a66d328e2b6424ec83e2afefae86f33
shaker <shakalghaithi@gmail.com>
2014-06-25 11:17:07 -0700

    pushing the sulotion to github

--- lab/lib/randomarraygenerator.rb
+++ lab/lib/randomarraygenerator.rb
@@ -1,7 +1,13 @@
 module RandomArrayGenerator
   def self.generate_random_array(size, min, max)
-
-    #TODO: Generate an array of (size) random integers (Between min and max inclusive)
-
+    arr = []
+    if size < 0 || min > max
+    return nil
+    else
+    size.times do
+    arr.push rand(min..max)
+    end
+    end
+    return arr
   end
 end
--- lab/lib/traineegrocerybagger.rb
+++ lab/lib/traineegrocerybagger.rb
@@ -2,14 +2,20 @@ module TraineeGroceryBagger
 
   MAX_BAG_WEIGHT = 10
 
-  def self.pack items
+  def self.pack(items_array)
+    if items_array = nil
+      return nil
+    elsif items_array.length == 0
+      return nil
+    end
     bags = nil
+    bags = []
+    bag_items = {}
+    bags << bag_itmes
+    items_array.each do |x|
+      bags << bag_items[x]
 
-    #TODO: Put all the items in bags with no bag weighing more than MAX_BAG_WEIGHT
-    #Return the Bags in an array
-    #Return each bag as an array of items
-    #Note: there's no need to balance or optimize the bagging
-
+    end
     bags
   end
 
